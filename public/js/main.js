c10864276527496aa72262b6605f726dd3c2a8da
Bilal Guerfa <GuerfaBilal@outlook.com>
2020-09-07 10:42:37 +0200

    exo test

---
+++ public/js/main.js
@@ -0,0 +1,120 @@
+// // // 2
+// // let maBoolean = false;
+// // // 3
+// // var monArray = [];
+// // // 4
+// // let monObjet = {
+
+// // };
+// // // 5
+// // let string = "chien";
+// // // 6
+// // let integer = 13;
+// // // 7
+// // let objet = {
+// //     seDeplacer(){
+
+// //     }
+// // };
+// // // 8
+// // const saluer = (name, age) => {
+    
+// // };
+// // // 9
+// // let monInt = 5;
+// // if (monInt < 8) {
+    
+// // };
+// // // 10
+// // for (let index = 0; index <= 9; index++) {
+    
+    
+// // };
+// // // 11
+// // let date = new Date;
+// // console.log(date);
+// // // 12
+// // let string2 = "chien";
+// // string2 = string2.toUpperCase();
+// // console.log(string2);
+// // // 13
+// // let string3 = "chien";
+// // string3 = string3.substr(0,1).toUpperCase() + string3.substr(1,3).toLowerCase() + string3.substr(-1,1).toUpperCase();
+// // console.log(string3);
+// // // 14
+// // const length = (text) =>{
+// //     let length = text.length;
+// //     return length;
+
+// // }
+// // // 15
+// // const aléatoire = () =>{
+// //     console.log(Math.random()*100);
+// // }
+// // // 16
+// // const aléatoire2 = (chiffre1, chiffre2) =>{
+// //     alert(Math.pow(chiffre1, chiffre2));
+// // }
+// // aléatoire2(25, 85);
+// // // 17
+// // const type = (valeur) =>{
+// //     if (valeur = String) {
+// //         alert("Votre valeur est de type String");
+// //     }
+// //     else if (valeur = Number){
+// //         alert("Votre valeur est de type Number");
+
+// //     }
+// //     else if (valeur = Array) {
+// //         alert("Votre valeur est de type Array");
+// //     }
+// //     else if (valeur = Boolean) {
+// //         alert("Votre valeur est de type Boolean");
+// //     }
+// //     else{
+// //         alert("Votre valeur est de type Objet");
+// //     }
+
+
+// // }
+// // type("chien");
+// // 18
+// // let objet = {
+// //     nom: [],
+// //     age: [],
+// //     modeling(nom1, age1){
+// //         objet.nom.push(nom1);
+// //         objet.age.push(age1);
+// //         alert(`Bonjour ${objet.nom[0]} tu as ${objet.age[0]} ans`)
+
+// //     }
+
+
+// // }
+// // objet.modeling("Bilal", 22)
+// // // 19
+// // let objet2 = {
+// //     addkey(){
+// //         objet.taille = 125;
+// //         console.log(objet.taille);
+
+// //     }
+// // }
+// // objet2.addkey()
+// // 20
+// let carre = {
+//     size: [],
+//     borderaspect: [],
+//     epaisseur: [],
+//     couleur: [],
+//     rajout(size1, borderaspect1, epaisseur1, couleur1){
+//         this.size.push(size1);
+//         this.borderaspect.push(borderaspect1)
+//     }
+
+
+// }
+// Pas le temps de terminer...
+
+
+
